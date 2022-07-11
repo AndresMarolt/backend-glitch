8bb0442334ad0adcf182a60455f42f88880868ca
AndresMarolt <68861100+AndresMarolt@users.noreply.github.com>
2022-07-11 20:11:27 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const path = require('path');
+const rutasProducto = require('../routes/rutasProducto');
+const rutasCarrito = require('../routes/rutasCarrito');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/api/productos', rutasProducto);
+app.use('/api/carrito', rutasCarrito);
+app.get('*', (req, res) => {
+    res.status(404).send({error: 404, descripcion: 'Ruta inexistente'});
+  });
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log(`Error: ${err}`);
+    } else {
+        console.log(`Server listening to port ${port}`)
+    }
+})
